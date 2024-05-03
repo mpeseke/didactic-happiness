6604c89f76be614745897f7a761b5f9cad008d7a
Marlon Peseke <mpeseke@gmail.com>
2024-05-02 23:42:02 -0600

    feat(api): added source api for exercises

---
+++ src/api/exercisesApi.ts
@@ -0,0 +1,9 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const getExercisesByBodyPart = () => {
+
+    console.log(API_KEY);
+    console.log(API_URL);
+
+}
